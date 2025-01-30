69eeedd661a6e1442b062eb0efd8336ea58e92c6
tea-tya <yamanaka6020@gmail.com>
2025-01-30 09:15:23 +0900

    M3-3まで
    敵の表示、移動

--- pyxel_game.py
+++ pyxel_game.py
@@ -3,14 +3,16 @@ import pyxel  #pyxelモジュールをインポートする
 PLAYER_SPEED = 2  #プレイヤーのスピードを定数にする
 
 def __init__():  #ゲームの初期設定をする関数を定義する
-    global player_x, player_y  #プレイヤーのx座標、y座標をグローバル化
+    global player_x, player_y, enemy_x, enemy_y  #全ての変数をグローバル化
     pyxel.init(160,120)  #ゲーム画面の縦、横の幅を決める
     pyxel.load('my_resource.pyxres')  #絵や音楽のデータを読み込む
     player_x, player_y = 0, 0  #ゲーム開始時の(プレイヤーのx座標,y座標)を(0,0)にする
+    enemy_x, enemy_y = 100, 100  #ゲーム開始時の(敵のx座標,y座標)を(100,100)にする
     pyxel.run(update, draw)  #動き(プレイヤーの座標など)の更新と、描画をし続ける
 
 def update():  #動き(プレイヤーの座標など)の更新をする関数を定義する
     global player_x, player_y  #プレイヤーのx座標、y座標をグローバル化
+    global enemy_x, enemy_y
     if pyxel.btn(pyxel.KEY_W):  #もしWが押されたら(長押し可能)
         player_y = player_y - PLAYER_SPEED  #プレイヤーのy座標を2減らす(上に2動く)
     elif pyxel.btn(pyxel.KEY_S):
@@ -27,9 +29,13 @@ def update():  #動き(プレイヤーの座標など)の更新をする関数�
         player_y = pyxel.height - 16
     if player_y < 0:
         player_y = 0
+    enemy_x = enemy_x - 1  #敵は1フレームで右に1移動させる
+    if enemy_x < -10:  #左側で見切れたら
+        enemy_x = pyxel.width  #右端に戻す
 
 def draw():  #描画をする関数を定義する
     pyxel.cls(0)  #画面を黒色(色番号:0)でリセットする
     pyxel.blt(player_x, player_y, 0, 0, 0, 10, 16)  #x座標がplayer_x(変数),y座標がplayer_yの位置に、パレット0番の0,0から横10、縦16ピクセルの絵を表示する
+    pyxel.rect(enemy_x, enemy_y, 10, 10, 3)
 
 __init__()  #ゲームを実行する
--- pyxel_game_no_comment.py
+++ pyxel_game_no_comment.py
@@ -3,14 +3,16 @@ import pyxel
 PLAYER_SPEED = 2
 
 def __init__():
-    global player_x, player_y
+    global player_x, player_y, enemy_x, enemy_y
     pyxel.init(160,120)
     pyxel.load('my_resource.pyxres')
     player_x, player_y = 0, 0
+    enemy_x, enemy_y = 100, 100
     pyxel.run(update, draw)
 
 def update():
     global player_x, player_y
+    global enemy_x, enemy_y
     if pyxel.btn(pyxel.KEY_W):
         player_y = player_y - PLAYER_SPEED
     elif pyxel.btn(pyxel.KEY_S):
@@ -27,9 +29,13 @@ def update():
         player_y = pyxel.height - 16
     if player_y < 0:
         player_y = 0
+    enemy_x = enemy_x - 1
+    if enemy_x < -10:
+        enemy_x = pyxel.width
 
 def draw():
     pyxel.cls(0)
     pyxel.blt(player_x, player_y, 0, 0, 0, 10, 16)
+    pyxel.rect(enemy_x, enemy_y, 10, 10, 3)
 
 __init__()
