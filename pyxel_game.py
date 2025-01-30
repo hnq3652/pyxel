145a3532863522ffab55c59c5152f234e29e497d
tea-tya <yamanaka6020@gmail.com>
2025-01-30 10:14:29 +0900

    M4-1まで
    当たり判定、敵の大きさを変更

--- pyxel_game.py
+++ pyxel_game.py
@@ -3,16 +3,21 @@ import pyxel  #pyxelモジュールをインポートする
 PLAYER_SPEED = 2  #プレイヤーのスピードを定数にする
 
 def __init__():  #ゲームの初期設定をする関数を定義する
-    global player_x, player_y, enemy_x, enemy_y  #全ての変数をグローバル化
+    global player_x, player_y, enemy_x, enemy_y, player_alive  #全ての変数をグローバル化
     pyxel.init(160,120)  #ゲーム画面の縦、横の幅を決める
     pyxel.load('my_resource.pyxres')  #絵や音楽のデータを読み込む
     player_x, player_y = 0, 0  #ゲーム開始時の(プレイヤーのx座標,y座標)を(0,0)にする
     enemy_x, enemy_y = 100, 100  #ゲーム開始時の(敵のx座標,y座標)を(100,100)にする
+    player_alive = True  #プレイヤーが生きているかのフラグ変数をTrue(生きている)にする
     pyxel.run(update, draw)  #動き(プレイヤーの座標など)の更新と、描画をし続ける
 
 def update():  #動き(プレイヤーの座標など)の更新をする関数を定義する
+    update_player()  #プレイヤーの更新
+    update_enemy()  #敵の更新
+    check_collision()  #当たり判定
+
+def update_player():
     global player_x, player_y  #プレイヤーのx座標、y座標をグローバル化
-    global enemy_x, enemy_y
     if pyxel.btn(pyxel.KEY_W):  #もしWが押されたら(長押し可能)
         player_y = player_y - PLAYER_SPEED  #プレイヤーのy座標を2減らす(上に2動く)
     elif pyxel.btn(pyxel.KEY_S):
@@ -29,13 +34,24 @@ def update():  #動き(プレイヤーの座標など)の更新をする関数�
         player_y = pyxel.height - 16
     if player_y < 0:
         player_y = 0
+
+def update_enemy():
+    global enemy_x, enemy_y
     enemy_x = enemy_x - 1  #敵は1フレームで右に1移動させる
     if enemy_x < -10:  #左側で見切れたら
         enemy_x = pyxel.width  #右端に戻す
 
+def check_collision():  #当たり判定は、各座標についてenemy-player_size < player < enemy+enemy_sizeで判定
+    global player_alive
+    if enemy_x-10 < player_x < enemy_x+8 and enemy_y-16 < player_y < enemy_y+8:  #x座標が重なっている、かつy座標が重なっているなら
+        player_alive = False  #プレイヤーが生きているかをFalse(死んでいる)にする
+
 def draw():  #描画をする関数を定義する
     pyxel.cls(0)  #画面を黒色(色番号:0)でリセットする
-    pyxel.blt(player_x, player_y, 0, 0, 0, 10, 16)  #x座標がplayer_x(変数),y座標がplayer_yの位置に、パレット0番の0,0から横10、縦16ピクセルの絵を表示する
-    pyxel.rect(enemy_x, enemy_y, 10, 10, 3)
+    if player_alive:  #プレイヤーが生きているなら
+        pyxel.blt(player_x, player_y, 0, 0, 0, 10, 16)  #x座標がplayer_x(変数),y座標がplayer_yの位置に、パレット0番の0,0から横10、縦16ピクセルの絵を表示する
+    else:
+        pyxel.blt(player_x, player_y, 0, 0, 0, 10, -16)  #プレイヤーが死んだら絵を上下反転させる
+    pyxel.rect(enemy_x, enemy_y, 8, 8, 3)
 
 __init__()  #ゲームを実行する
--- pyxel_game_no_comment.py
+++ pyxel_game_no_comment.py
@@ -3,16 +3,21 @@ import pyxel
 PLAYER_SPEED = 2
 
 def __init__():
-    global player_x, player_y, enemy_x, enemy_y
+    global player_x, player_y, enemy_x, enemy_y, player_alive
     pyxel.init(160,120)
     pyxel.load('my_resource.pyxres')
     player_x, player_y = 0, 0
     enemy_x, enemy_y = 100, 100
+    player_alive = True
     pyxel.run(update, draw)
 
 def update():
+    update_player()
+    update_enemy()
+    check_collision()
+
+def update_player():
     global player_x, player_y
-    global enemy_x, enemy_y
     if pyxel.btn(pyxel.KEY_W):
         player_y = player_y - PLAYER_SPEED
     elif pyxel.btn(pyxel.KEY_S):
@@ -29,13 +34,24 @@ def update():
         player_y = pyxel.height - 16
     if player_y < 0:
         player_y = 0
+
+def update_enemy():
+    global enemy_x, enemy_y
     enemy_x = enemy_x - 1
     if enemy_x < -10:
         enemy_x = pyxel.width
 
+def check_collision():
+    global player_alive
+    if enemy_x-10 < player_x < enemy_x+8 and enemy_y-16 < player_y < enemy_y+8:
+        player_alive = False
+
 def draw():
     pyxel.cls(0)
-    pyxel.blt(player_x, player_y, 0, 0, 0, 10, 16)
-    pyxel.rect(enemy_x, enemy_y, 10, 10, 3)
+    if player_alive:
+        pyxel.blt(player_x, player_y, 0, 0, 0, 10, 16)
+    else:
+        pyxel.blt(player_x, player_y, 0, 0, 0, 10, -16)
+    pyxel.rect(enemy_x, enemy_y, 8, 8, 3)
 
 __init__()
